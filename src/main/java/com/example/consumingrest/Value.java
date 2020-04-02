83dbfa69bb13899c359b742895fb4dc449d4e3c6
Natalia <natikrak@gmail.com>
2020-04-02 11:55:58 +0200

    exercises

---
+++ src/main/java/com/example/consumingrest/Value.java
@@ -0,0 +1,36 @@
+package com.example.consumingrest;
+
+public class Value {
+
+    private Long id;
+    private String quote;
+
+    public Value() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Value setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public Value setQuote(String quote) {
+        this.quote = quote;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Value{" +
+                "id=" + id +
+                ", quote='" + quote + '\'' +
+                '}';
+    }
+}
